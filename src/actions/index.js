3560f11eae611b403b1f09f3959ce6849e041446
glory1357 <efim133@mail.ru>
2022-02-01 16:02:37 +0300

    finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,7 +11,7 @@ export const storeClear = createAction('STORE_STORE_CLEAR');
 
 export const fetchWord = (name) => async (dispatch) => {
   dispatch(wordFetching());
-  await axios.get(`http://api.dictionaryapi.dev/api/v2/entries/en/${name}`)
+  await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${name}`)
     .then((res) => dispatch(wordFetched(res.data[0])))
     .catch((err) => dispatch(wordFetchingError(err.response ? err.response.status : true)));
 };
